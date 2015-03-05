4a207288e5d39c4742239582a9bec2410c7acc6d
Andrew Matthews <matthews.andrew@gmail.com>
2015-03-05 14:39:50 +1100

    blah

--- bootstrap/git-autocommit.sh
+++ bootstrap/git-autocommit.sh
@@ -8,6 +8,9 @@ pushd .
 cd $1
 # 2. stage any untracked changes
 # 3. stash the changes
+
+logger -t git stashing changes on: $1
+
 git stash
 
 popd
--- setup.zsh
+++ setup.zsh
@@ -1,3 +1,5 @@
+export GOPATH=/Users/andrewmatthews/dev/go
+export TPDIR=/Users/andrewmatthews/Dropbox
 export DOCKER_HOST=tcp://192.168.59.103:2376
 export DOCKER_CERT_PATH=/Users/andrewmatthews/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
@@ -7,6 +9,8 @@ export MAVEN_OPTS="-Xms256m -Xmx512m"
 export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 export PATH=$JAVA_HOME/bin:$M2:/usr/local/bin:$PATH
 export PATH="/usr/local/git/bin:/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/bin:/usr/local/apache-maven/apache-maven-3.2.2/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
+export PATH=$PATH:/usr/local/opt/go/libexec/bin
+export PATH=$PATH:$GOPATH/bin
 
 source $(brew --prefix nvm)/nvm.sh
 
@@ -23,7 +27,17 @@ export DATEFMT='%Y-%m-%d'
 export FINANCE_DIR='~/Dropbox/Finance'
 alias ac="ledger -f $FINANCE_DIR/crypto.ledger --date-format $DATEFMT -W --price-db $FINANCE_DIR/price.db"
 alias bal="ledger -f $FINANCE_DIR/crypto.ledger --price-db $FINANCE_DIR/price.db -w --flat bal"
-alias t='todo.sh'
+
+# ------------------------------------------------------------------------------------------------
+# Task management with todo.sh
+# ------------------------------------------------------------------------------------------------
+export TODOTXT_SORT_COMMAND='env LC_COLLATE=C sort -k 2,2 -k 1,1n'
+export TODOTXT_DEFAULT_ACTION=ls
+
+alias t='todo.sh -N'
+alias task='t add'
+alias tasks='t list'
+alias projects='t lsprj'
 
 # ------------------------------------------------------------------------------------------------
 # Simple time-saver for creating dir and then moving into it
