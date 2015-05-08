77c8aa9b1f066bcce0529ce34dcd384e049fcb01
Andrew Matthews <matthews.andrew@gmail.com>
2015-05-08 13:08:48 +1000

    misc path updates

--- setup.zsh
+++ setup.zsh
@@ -3,16 +3,19 @@ export TPDIR=/Users/andrewmatthews/Dropbox
 export DOCKER_HOST=tcp://192.168.59.103:2376
 export DOCKER_CERT_PATH=/Users/andrewmatthews/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
-export M2_HOME=/usr/local/apache-maven/apache-maven-3.2.2
-export M2=$M2_HOME/bin
+export M3_HOME=/usr/local/Cellar/maven/3.3.1
+export M3=$M2_HOME/bin
 export MAVEN_OPTS="-Xms256m -Xmx512m"
-export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
+# export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 export PATH=$JAVA_HOME/bin:$M2:/usr/local/bin:$PATH
-export PATH="/usr/local/git/bin:/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/bin:/usr/local/apache-maven/apache-maven-3.2.2/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
+export PATH="$M3:/usr/local/git/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin:/Users/andrewmatthews/repository/personal/dev/packer/packer_0.7.5_darwin_amd64"
 export PATH=$PATH:/usr/local/opt/go/libexec/bin
 export PATH=$PATH:$GOPATH/bin
+export MONO_GAC_PREFIX="/usr/local"
+source dnvm.sh
 
-source $(brew --prefix nvm)/nvm.sh
+# source $(brew --prefix nvm)/nvm.sh
 
 # ------------------------------------------------------------------------------------------------
 #  Some useful little aliases for tools I use a lot
@@ -53,12 +56,12 @@ alias mcd='make-cd'
 # ------------------------------------------------------------------------------------------------
 # Docker commands for cleaning up containers and images (needs work)
 # ------------------------------------------------------------------------------------------------
+boot2docker up
 
-docker-ip() {
-  boot2docker ip 2> /dev/null
-}
-
-alias rmdockercontainers="docker ps -a -q | xargs -n 1 -I {} docker rm {}"
+alias rmdockercontainers="docker rm $(docker ps -a -q)"
+alias rmdockerimages="docker rmi -f $(docker images -q)"
+alias dockercleanup="rmdockercontainers && rmdockerimages"
+# alias rmdockercontainers="docker ps -a -q | xargs -n 1 -I {} docker rm {}"
 # alias rmdockerimages="docker rmi -f $(docker images | grep 'ago' | tr -s ' ' | cut -d ' ' -f 3)"
 alias cleanupdocker="rmdockercontainers; rmdockerimages"
 
